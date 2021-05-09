262c891040146ef55007353ae3950b3f33b13e29
Tuomo Artama <tuomo.artama@gmail.com>
2021-05-09 22:19:51 +0300

    Schema.sql päivitetty

--- schema.sql
+++ schema.sql
@@ -1,1 +1,4 @@
 CREATE TABLE users (id SERIAL PRIMARY KEY, name TEXT, username TEXT, password TEXT, usergroup TEXT, email TEXT, phone TEXT);
+CREATE TABLE locations (id SERIAL PRIMARY KEY, name TEXT);
+CREATE TABLE jobs (id SERIAL PRIMARY KEY, name TEXT, time TEXT, location INT);
+CREATE TABLE participants (id SERIAL PRIMARY KEY, jobId INT, userId INT, status TEXT);
